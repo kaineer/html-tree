832164cb92a33c9fddbe87c13afee6b70f144f73
Sergey Kluchkovsky <kaineer@gmail.com>
2020-12-29 16:03:40 +0500

    Добавляет тесты

--- test/index-test.js
+++ test/index-test.js
@@ -35,4 +35,30 @@ describe('checkDOM()', () => {
     expect(messages[0].ruleId).to.eq('no-elements');
     expect(messages[0].el.tagName.toLowerCase()).to.eq('body');
   });
+
+  it('should fail if there is modifier outside element', () => {
+    const {ok, messages} = f(
+      `<html><body>
+         <div class="block">
+            <div class="block__element--with-modifier"></div>
+         </div>
+       </body></html>`);
+
+    expect(ok).to.eq(false);
+    expect(messages.length).to.eq(1);
+    expect(messages[0].ruleId).to.eq('modifier-out-of-element');
+    expect(messages[0].el.tagName.toLowerCase()).to.eq('div');
+  });
+
+  it('should fail if there is element outside block', () => {
+    const {ok, messages} = f(
+      `<html><body>
+         <div class="block__element"></div>
+       </body></html>`);
+
+    expect(ok).to.eq(false);
+    expect(messages.length).to.eq(1);
+    expect(messages[0].ruleId).to.eq('element-out-of-block');
+    expect(messages[0].el.tagName.toLowerCase()).to.eq('div');
+  });
 });
